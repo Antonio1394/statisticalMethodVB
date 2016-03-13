a761a93ed3fdd74e97d8ffbf39c1011f58fd8dbe
Antonio1394 <antoniorivm1394@gmail.com>
2016-03-12 23:02:36 -0600

    [add] dataGrid

---
+++ EstadisticaMatematica/FormResultados.Designer.vb
@@ -0,0 +1,77 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormResultados
+    Inherits Telerik.WinControls.UI.RadForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim GridViewTextBoxColumn1 As Telerik.WinControls.UI.GridViewTextBoxColumn = New Telerik.WinControls.UI.GridViewTextBoxColumn()
+        Me.TelerikMetroTouchTheme1 = New Telerik.WinControls.Themes.TelerikMetroTouchTheme()
+        Me.RadGridView1 = New Telerik.WinControls.UI.RadGridView()
+        CType(Me.RadGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadGridView1.MasterTemplate, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'RadGridView1
+        '
+        Me.RadGridView1.BackColor = System.Drawing.Color.White
+        Me.RadGridView1.Cursor = System.Windows.Forms.Cursors.Default
+        Me.RadGridView1.Font = New System.Drawing.Font("Segoe UI", 8.25!)
+        Me.RadGridView1.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.RadGridView1.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.RadGridView1.Location = New System.Drawing.Point(48, 104)
+        '
+        'RadGridView1
+        '
+        GridViewTextBoxColumn1.EnableExpressionEditor = False
+        GridViewTextBoxColumn1.HeaderText = "Intervalo"
+        GridViewTextBoxColumn1.Name = "column1"
+        Me.RadGridView1.MasterTemplate.Columns.AddRange(New Telerik.WinControls.UI.GridViewDataColumn() {GridViewTextBoxColumn1})
+        Me.RadGridView1.Name = "RadGridView1"
+        Me.RadGridView1.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.RadGridView1.Size = New System.Drawing.Size(662, 263)
+        Me.RadGridView1.TabIndex = 0
+        Me.RadGridView1.Text = "RadGridView1"
+        Me.RadGridView1.ThemeName = "TelerikMetro"
+        '
+        'FormResultados
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(749, 392)
+        Me.Controls.Add(Me.RadGridView1)
+        Me.Name = "FormResultados"
+        '
+        '
+        '
+        Me.RootElement.ApplyShapeToControl = True
+        Me.Text = "FormResultados"
+        Me.ThemeName = "TelerikMetroTouch"
+        CType(Me.RadGridView1.MasterTemplate, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TelerikMetroTouchTheme1 As Telerik.WinControls.Themes.TelerikMetroTouchTheme
+    Friend WithEvents RadGridView1 As Telerik.WinControls.UI.RadGridView
+End Class
+
